9b93716e5d4e9a9cc9abf15ad6027008d6778c7e
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-07-04 16:33:07 +0200

    add landind page

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <AppNavBar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style>
+
+</style>
