9eed155ae9763cd7b9b8882a7f9cb2bb4e6e4cee
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-07-05 16:44:16 +0200

    maybe this will help?

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,7 +9,9 @@ export const auth = betterAuth({
     provider: "sqlite",
   }),
   advanced: {
-    generateId: false,
+    database: {
+      generateId: false,
+    },
   },
   socialProviders: {
     github: {
