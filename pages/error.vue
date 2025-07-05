07a9b51bd8b8920ef7eb2967931f386055b1ac03
Mick Pal <iamnotlazzaroni@gmail.com>
2025-07-05 16:24:16 +0200

    As a guest, I want to log in using my GitHub account (#55)
    * add github env variables
    
    * add pinia & define auth store
    
    * handle auth flow + add error page
    
    * update workflow

---
+++ pages/error.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const route = useRoute();
+
+const error = computed(() => {
+  return route.query.error || "An unexpected error occurred.";
+});
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col justify-center items-center gap-4">
+    <div role="alert" class="alert alert-error min-w-1/2 text-center">
+      <span>Oh no! {{ error }}</span>
+    </div>
+    <NuxtLink to="/" class="btn btn-primary">
+      Home
+      <Icon name="tabler:arrow-left" size="24" />
+    </NuxtLink>
+  </div>
+</template>
+
+<style>
+
+</style>
