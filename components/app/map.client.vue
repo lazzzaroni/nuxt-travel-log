cb1bf08e48b25f1e08f71ea6f44d85a2912e6753
Mick Pal <iamnotlazzaroni@gmail.com>
2025-07-10 15:35:19 +0200

    add base map to dashboard (#66)
    * Map component library is setup / configured
    
    * add base map to dashboard

---
+++ components/app/map.client.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { LODZ } from "~/lib/constants";
+
+// maputnik - to style tiles
+
+const colorMode = useColorMode();
+
+const style = computed(() => {
+  return colorMode.value === "dark"
+    ? "/styles/dark.json"
+    : "https://tiles.openfreemap.org/styles/liberty";
+});
+const zoom = 6;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="LODZ"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
+
+<style>
+
+</style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -81,8 +81,9 @@ onMounted(() => {
         />
       </div>
     </div>
-    <div class="flex-1">
+    <div class="flex-1 flex flex-col">
       <NuxtPage />
+      <AppMap class="flex-1" />
     </div>
   </div>
 </template>
